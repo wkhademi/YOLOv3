4173bf368cb5fe3fa2f5c930a771b694a80ad19c
Wkhademi <khademi.wesley9@gmail.com>
2018-12-24 17:36:44 -0800

    Data preprocessing pipeline almost finished. Finished resizing of images and bounding boxes. Need to add resave to directory

--- prepare.py
+++ prepare.py
@@ -63,17 +63,17 @@ class PrepareData:
     def next(self):
         self.batch += 1
         if (self.batch <= self.max_batch):
-            if (self.image_num + int(self.model_info["batch_size"]) > self.length): # partial batch
-                self.load_data(self.image_num, self.length)
-                self.image_num += (self.length - self.image_num)
-            else: # full batch
-                self.load_data(self.image_num, self.image_num + int(self.model_info["batch_size"]))
-                self.image_num += int(self.model_info["batch_size"])
+            if (self.image_num + int(self.model_info["batch_size"]) > self.length):
+                increment = self.length - self.image_num
+            else:
+                increment = int(self.model_info["batch_size"])
 
+            self.load_data(self.image_num, self.image_num + increment)
             self.resize_images(int(self.dataset_info["height"]), int(self.dataset_info["width"]))
+            self.resize_bounding_boxes(self.image_num, self.image_num + increment)
+            self.image_num += increment
         else:
             raise StopIteration
-
 
     def load_data(self, start, finish):
         """
@@ -87,32 +87,68 @@ class PrepareData:
 
         self.images = np.asarray(images)
 
-
     def resize_images(self, max_height, max_width):
         """
             Downsample all images to a specific max height and width.
         """
+        self.scales = []
+        self.horizontal_shifts = []
+        self.vertical_shifts = []
+
         for idx, image in enumerate(self.images):
             height, width, _ = image.shape
 
-            if max_height < height or max_width < width:
-                scaling_factor = max_height / float(height)
+            if (max_height < height or max_width < width):
+                scaling_factor = min(max_height / float(height), max_width / float(width))
+                self.scales.append(scaling_factor)
 
-                if (max_width / float(width) < scaling_factor):
-                    scaling_factor = max_width / float(width)
-
+                # resize image
                 self.images[idx] = cv2.resize(image, None, fx=scaling_factor, fy=scaling_factor, interpolation=cv2.INTER_AREA)
 
+                # pad images so they are 416x416
+                height, width, _ = self.images[idx].shape
+                longest_edge = max(height, width)
+                top, bottom, left, right = (0, 0, 0, 0)
+                BLACK = [0, 0, 0]
 
-    def preprocess_images(self):
-        """
-            Perform random data augmentation on image set.
-        """
-        pass
+                if (height < longest_edge):
+                    dh = longest_edge - height
+                    top = dh // 2
+                    bottom = dh - top
+                    self.vertical_shifts.append(top)
+                    self.horizontal_shifts.append(left)
+                elif (width < longest_edge):
+                    dw = longest_edge - width
+                    left = dw // 2
+                    right = dw - left
+                    self.vertical_shifts.append(top)
+                    self.horizontal_shifts.append(left)
+                else:
+                    self.vertical_shifts.append(top)
+                    self.horizontal_shifts.append(left)
 
+                self.images[idx] = cv2.copyMakeBorder(self.images[idx], top, bottom, left, right, cv2.BORDER_CONSTANT, value=BLACK)
+            else:
+                top, left = 0
+                scaling_factor = 1
+                self.scales.append(scaling_factor)
+                self.vertical_shifts.append(top)
+                self.horizontal_shifts.append(left)
 
-    def resize_bounding_boxes(self):
+    def resize_bounding_boxes(self, start, finish):
         """
             Resize the labeled bounding boxes to match the downsampled images.
         """
-        pass
+        keys = self.image_info.keys()
+        for idx, image_path in enumerate(keys[start:finish]):
+            height, width, _ = self.images[idx].shape
+            image_info = self.image_info[image_path]
+            scaling_factor = self.scales[idx]
+
+            # scale and shift all bounding boxes
+            for index, data in enumerate(image_info):
+                bbox = data[0]
+                self.image_info[image_path][index][0]["xmin"] = int(np.round(bbox["xmin"] * scaling_factor)) + self.horizontal_shifts[idx]
+                self.image_info[image_path][index][0]["ymin"] = int(np.round(bbox["ymin"] * scaling_factor)) + self.vertical_shifts[idx]
+                self.image_info[image_path][index][0]["ymax"] = int(np.round(bbox["ymax"] * scaling_factor)) + self.vertical_shifts[idx]
+                self.image_info[image_path][index][0]["xmax"] = int(np.round(bbox["xmax"] * scaling_factor)) + self.horizontal_shifts[idx]
