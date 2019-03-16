2818236d472387085e9bc059c2f7b3c366e0e077
srikar <srikarp18@gmail.com>
2019-03-16 14:06:46 -0700

    .

--- display.py
+++ display.py
@@ -22,8 +22,25 @@ def display_video():
     ret = capture.set(3, 640)
     ret = capture.set(4, 480)
 
+    counter = 0;
+    seconds = 0;
+    frame_rate = 0;
     while True:
+        if(counter == 0):
+            start = time.time()
+        elif(counter == 10):
+            end = time.time()
+            seconds = start - end
+            frame_rate = 10/seconds
+            counter = -1
         ret, frame = capture.read()
+        counter += 1
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        font_size = 1
+        font_color = (255, 255, 255)
+        font_weight = 2
+        font_line = cv2.LINE_AA
+        cv2.putText(frame, str(frame_rate), (10, 10), font, font_size, font_color, font_weight, font_line)
 
         # predict_bounding_boxes function currently not implemented...
         obj_det_frame = predict_bounding_boxes(frame)
